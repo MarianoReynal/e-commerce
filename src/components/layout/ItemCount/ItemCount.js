cc3d1fb908415f737dfec022a1e71c5540b7c4bc
Mariano Reynal <mariano_alejandroreynal@hotmail.com>
2021-12-13 11:22:11 -0300

    .

--- src/components/layout/ItemCount/ItemCount.js
+++ src/components/layout/ItemCount/ItemCount.js
@@ -1,27 +1,19 @@
 import React from 'react'
-import { BtnConfig } from './BtnConfig'
+import { btnConfig } from './btnConfig'
 
-export const ItemCount = ( {max, setCantidad, cantidad, onAdd} ) => {
+export const ItemCount = ( {sumar, restar, cantidad, onAdd, max} ) => {
 
-    const handleRestar = () => {
-        cantidad > 0 && setCantidad(cantidad - 1)
-    }
-
-    const handleSumar = () => {
-        cantidad < max && setCantidad(cantidad + 1)
-    }
-
-    const config = BtnConfig(cantidad, max, handleSumar, handleRestar)
+    const config = btnConfig(cantidad, max)
 
     return (
         <div className="my-3">
-            <button {...config.restar}>
+            <button {...config.restar} onClick={restar}>
                 -
             </button>
 
             <span className="mx-2">{cantidad}</span>
 
-            <button {...config.sumar}>
+            <button {...config.sumar} onClick={sumar}>
                 +
             </button>
             <br/>
@@ -35,52 +27,3 @@ export const ItemCount = ( {max, setCantidad, cantidad, onAdd} ) => {
         </div>
     )
 }
-
-
-/*
-import React,{useState} from "react";
-
-
-const stock = 10 ;
-export const ItemCount = () =>{
-   
-    const[counter,setCounter] = useState(1);
-
-    const incrementar = () =>{
-        if(stock>counter){
-        setCounter(counter+1);
-        }else{
-            alert ("no hay mas stock")
-        }
-    }
-
-
-    const decrementar = () =>{
-        if(counter>1){
-        setCounter(counter-1);
-        }else{
-            alert ("no se puede disminuir mas")
-        }
-    }
-    
-    const respuesta = () =>{
-        alert (counter)
-    }
-
-
-
-return(
-<div>
-    <strong>{counter}</strong>
-    <div>
-        <button className="btn btn-primary"  onClick={decrementar}>-</button>
-        <button className="btn btn-primary"  onClick={incrementar}>+</button>
-    </div>
-        <button className="btn btn-primary"  size="lg" onClick={respuesta}>
-         Agregar al carro
-         </button>
-</div>
-
-)
-}
-*/
