49f638e1092cf461d938b34edf433be9a315d818
Mariano Reynal <mariano_alejandroreynal@hotmail.com>
2021-12-16 15:43:51 -0300

    .

--- src/components/layout/CartView/CartItem.js
+++ src/components/layout/CartView/CartItem.js
@@ -7,16 +7,34 @@ export const CartItem = ({name, price, cantidad, id}) => {
     const {removerDelCarrito} = useContext(CartContext)
 
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Precio: ${price}</p>
-            <p>Cantidad: {cantidad}</p>
-            <button 
-                className="btn btn-danger"
-                onClick={() => { removerDelCarrito(id) }}
-            >
-                <BsFillTrashFill/>
-            </button>
+        <div className='container' >
+             <table className='table table-hover table-bordered table-sm '>   
+                <thead>
+                    <tr>
+                        <th>PRODUCTO</th>
+                        <th>PRECIO</th>
+                        <th>CANTIDAD</th>
+                        <th>#</th>
+                    </tr>
+                </thead>
+                <tbody>   
+                    <tr>
+                        <td>{name}</td>
+                        <td>${price}</td>
+                        <td>{cantidad}</td>
+                        <td>
+                        <button className="btn btn-danger"
+                                onClick={() => { removerDelCarrito(id) }}
+                            >
+                                <BsFillTrashFill/>
+                            </button>
+                        </td>
+                    </tr>
+                   
+                
+                </tbody> 
+            
+            </table>
         </div>
     )
 }
